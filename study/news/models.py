61e57d04054638acca3f9be14a285bca5864e8f5
YourBunnyWrote0000 <57093177+YourBunnyWrote0000@users.noreply.github.com>
2021-05-09 21:09:51 +0300

    DB
    Make migrations
    Added first post to DB by using admin panel

--- study/news/models.py
+++ study/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField('Name', max_length=50)
+    anons = models.CharField('Anons', max_length=250)
+    full_text = models.TextField('Text')
+    date = models.DateTimeField('Date of publication')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
